8761d433d73f26b53b8da662e416591442b0c0ed
Alexandr Vasilev <alxvasilev81@gmail.com>
2026-01-29 16:08:02 +0300

    Init Gradle app, add Hello World and dependencyUpdates

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
