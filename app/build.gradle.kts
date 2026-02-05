abad1a7adadd09024e4601cabb5a56729fefccdd
Alexandr Vasilev <alxvasilev81@gmail.com>
2026-02-05 15:58:42 +0300

    Add SonarQube configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     id("java")
     application
     id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.sonarqube") version "7.1.0.6387"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "AlxVS1981_java-project-61")
+        property("sonar.organization", "alxvs1981")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "org.example"
